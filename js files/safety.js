5dd20e2f0ccd0fcce5b15ca04c157949bf8613cf
GrimCreeperyt <grimcreeperyt@gmail.com>
2025-02-24 00:05:05 -0500

    Added complete navbars to every page, finished Safety Info.html

--- js files/safety.js
+++ js files/safety.js
@@ -1,4 +1,4 @@
-/*const app = Vue.createApp({
+const app = Vue.createApp({
   data() {
       return {
           cards: [] // Will hold card data from JSON
@@ -28,58 +28,4 @@
           .catch(error => console.error("Error loading data:", error));
   }
 });
-app.mount("#app");*/
-
-const isRTLCheck = (text) => {
-  return /[\u0590-\u05FF\u0600-\u06FF\u0700-\u074F]/.test(text);
-};
-
-document.querySelectorAll('.file-container').forEach((container) => {
-  const text = container.querySelector('.textV');
-  const textEffect = container.querySelector('.text-effect');
-  const title = container.querySelector('.titleV');
-  const description = container.querySelector('.description');
-  const borderColor = container.dataset.borderColor;
-  const hoverTextColor = container.dataset.hoverTextColor;
-
-  container.style.setProperty('--border-color', borderColor);
-  container.style.setProperty('--hover-text-color', hoverTextColor);
-
-  container.style.setProperty('--borderGradient', `conic-gradient(from var(--rotation), 
-                var(--border-color) 0deg, 
-                var(--border-color) 90deg, 
-                var(--color-4) 90deg, 
-                var(--color-4) 360deg)`);
-  container.style.backgroundImage = `linear-gradient(#000, #000), var(--borderGradient)`;
-  textEffect.style.backgroundColor = borderColor;
-
-  // Check direction for title and description
-  let isRTL = false;
-
-  if (isRTLCheck(title.textContent)) {
-    title.style.direction = 'rtl';
-    isRTL = true;
-  } else {
-    title.style.direction = 'ltr';
-  }
-
-  if (isRTLCheck(description.textContent)) {
-    description.style.direction = 'rtl';
-    isRTL = true;
-  } else {
-    description.style.direction = 'ltr';
-  }
-
-  // Change font family if RTL is detected in the container
-  if (isRTL) {
-    container.style.fontFamily = "'Montserrat', sans-serif";
-  }
-
-  container.addEventListener('mousemove', (e) => {
-    const rect = container.getBoundingClientRect();
-    const x = e.clientX - rect.left - rect.width / 2;
-    const y = e.clientY - rect.top - rect.height / 2;
-    const angle = Math.atan2(y, x);
-    container.style.setProperty("--rotation", `${angle}rad`);
-  });
-});
+app.mount("#app");
